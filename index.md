a87af2b51376565ec215fffc4119284937312599
mose <mose@mose.com>
2013-03-21 20:29:01 +0800

    some greeniness

---
+++ index.md
@@ -0,0 +1,10 @@
+---
+layout: default
+title: Home
+---
+
+* [{{ site.title }}](index.html)
+* [on github](https://github.com/mose)
+* [on twitter](http://twitter.com/mo5e)
+* [on linkedin](http://linkedin.com/in/ozmose)
+* [on tumblr](http://blog.mose.com)
---
+++ js/site.js
@@ -0,0 +1,31 @@
+// nothing there yet
+window.addEventListener('DOMContentLoaded', function() {
+
+  var body = document.getElementById('body');
+  var items = document.getElementsByTagName('li');
+
+  function display() {
+    w = window.innerWidth;
+    h = window.innerHeight;
+    big = (w < h) ? h : w;
+    n = Math.round(big / items.length);
+    if (n < 150) {
+      n = 150;
+      h = Math.round(h/2);
+    }
+    console.log(n);
+    for (i=0;i<items.length;i++) {
+
+      items[i].style.width = (n-11)+'px';
+      items[i].style.height = (h-12)+'px';
+      items[i].childNodes[0].style.width = (n-11)+'px';
+      items[i].childNodes[0].style.height = (h/2)+'px';
+      items[i].childNodes[0].style.fontSize = (n/10)+'px';
+      items[i].childNodes[0].style.lineHeight = (n/10)+'px';
+      items[i].childNodes[0].style.paddingTop = (h/2-n/10)+'px';
+    }
+  }
+
+  display();
+  window.onresize = function(){ display() };
+});
