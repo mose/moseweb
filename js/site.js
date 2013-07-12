e716b907ed3ad56727f861885599dd57706ddedb
mose <mose@mose.com>
2013-07-12 12:42:12 +0800

    fix language transition

--- js/site.js
+++ js/site.js
@@ -3,26 +3,31 @@ window.addEventListener('DOMContentLoaded', function() {
 
   var languages = ["en", "fr", "zh"];
   var language = window.navigator.userLanguage || window.navigator.language;
-
-  if (language != undefined) {
-    if (language.indexOf('-') > -1) {
-      language = language.substr(0,language.indexOf('-'));
-    }
-    if (languages.indexOf(language) == -1) {
-      language = 'en';
-    }
-    var here = window.location.href;
-    here = here.substr(here.lastIndexOf('/')+1);
-    if ((here == 'index.html' || here == '') && language != 'en') {
-      window.location.href = "index-"+language+".html";
-    }
-  }
-
+  var userlang = window.location.hash;
   var openlink = document.getElementById("open");
   var body = document.getElementById("body");
-  addEvent(openlink, 'click', function(e) {
-    e.preventDefault();
+
+  if (userlang === "#d") {
     removeClass(body,"full");
-  });
+  } else {
+    if (language != undefined) {
+      if (language.indexOf('-') > -1) {
+        language = language.substr(0,language.indexOf('-'));
+      }
+      if (languages.indexOf(language) == -1) {
+        language = 'en';
+      }
+      var here = window.location.href;
+      here = here.substr(here.lastIndexOf('/')+1);
+      if ((here == 'index.html' || here == '') && language != 'en') {
+        window.location.href = "index-"+language+".html";
+      }
+    }
+
+    addEvent(openlink, 'click', function(e) {
+      e.preventDefault();
+      removeClass(body,"full");
+    });
+  }
 
 });
